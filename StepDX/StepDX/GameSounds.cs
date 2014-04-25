2e9b15838a36db67bca07e431e34cbadfe3a5a18
Aaron Torres <torresaa@msu.edu>
2014-04-24 21:29:01 -0400

    Now using embedded audio files.  No lag.

--- StepDX/StepDX/GameSounds.cs
+++ StepDX/StepDX/GameSounds.cs
@@ -5,60 +5,92 @@ using System.Windows.Forms;
 
 using Microsoft.DirectX;
 using Microsoft.DirectX.DirectSound;
-using System.Media;
+
+using System.Reflection;
+using System.IO;
 
 namespace StepDX
 {
     class GameSounds
     {
-        System.IO.Stream jump;
-        System.IO.Stream die;
-        System.IO.Stream gameover;
-        System.IO.Stream gamewon;
-        System.IO.Stream kick;
+        private Device SoundDevice = null;
 
-        SoundPlayer player;
-
-        System.Reflection.Assembly assembly; 
+        private SecondaryBuffer jump = null;
+        private SecondaryBuffer die = null;
+        private SecondaryBuffer gameend = null;
+        private SecondaryBuffer gamewon = null;
+        private SecondaryBuffer kick = null;
 
         public GameSounds(Form form)
         {
-            assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            SoundDevice = new Device();
+            SoundDevice.SetCooperativeLevel(form, CooperativeLevel.Priority);
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            Load(ref jump, assembly.GetManifestResourceStream("StepDX.jump.wav"));
+            Load(ref die, assembly.GetManifestResourceStream("StepDX.mariodie.wav"));
+            Load(ref gameend, assembly.GetManifestResourceStream("StepDX.die.wav"));
+            Load(ref gamewon, assembly.GetManifestResourceStream("StepDX.stage_clear.wav"));
+            Load(ref kick, assembly.GetManifestResourceStream("StepDX.kick.wav"));
+        }
+
+        private void Load(ref SecondaryBuffer buffer, Stream stream)
+        {
+            try
+            {
+                buffer = new SecondaryBuffer(stream, SoundDevice);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unable to load " + stream,
+                                "Danger, Will Robinson", MessageBoxButtons.OK);
+                buffer = null;
+            }
         }
 
         public void Jump()
         {
-            jump = assembly.GetManifestResourceStream("StepDX.jump.wav");
-            player = new SoundPlayer(jump);
-            player.Play();
+            if (jump == null)
+                return;
+
+            if (!jump.Status.Playing)
+                jump.Play(0, BufferPlayFlags.Default);
         }
 
         public void Die()
         {
-            die = assembly.GetManifestResourceStream("StepDX.mariodie.wav");
-            player = new SoundPlayer(die);
-            player.Play();
+            if (die == null)
+                return;
+
+            if (!die.Status.Playing)
+                die.Play(0, BufferPlayFlags.Default);
         }
 
         public void GameOver()
         {
-            gameover = assembly.GetManifestResourceStream("StepDX.die.wav"); 
-            player = new SoundPlayer(gameover);
-            player.Play();
+            if (gameend == null)
+                return;
+
+            if (!gameend.Status.Playing)
+                gameend.Play(0, BufferPlayFlags.Default);
         }
 
         public void GameWon()
         {
-            gamewon = assembly.GetManifestResourceStream("StepDX.stage_clear.wav");
-            player = new SoundPlayer(gamewon);
-            player.Play();
+            if (gamewon == null)
+                return;
+
+            if (!gamewon.Status.Playing)
+                gamewon.Play(0, BufferPlayFlags.Default);
         }
 
         public void Kick()
         {
-            kick = assembly.GetManifestResourceStream("StepDX.kick.wav");
-            player = new SoundPlayer(kick);
-            player.Play();
+            if (kick == null)
+                return;
+
+            if (!kick.Status.Playing)
+                kick.Play(0, BufferPlayFlags.Default);
         }
     }
 }
